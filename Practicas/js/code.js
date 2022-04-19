2327e792a1cf60673c506a555f8df2569d7e4d2c
Carlos Francisco <86804652+calitoozzy@users.noreply.github.com>
2022-04-19 10:30:12 -0300

    repositorio general ToN

---
+++ Practicas/js/code.js
@@ -0,0 +1,52 @@
+
+/*
+var nota1 = prompt("ingrese la 1 nota");
+var nota2 = prompt("ingrese la 2 nota");
+var promedio = (parseFloat(nota1) + parseFloat(nota2)) /2;
+
+if (nota1&&nota2 > 10){
+    alert("ingrasar num menor o = 10 en la nota");
+}
+else {
+
+    if (promedio < 7) {
+        alert("Usted reprobo " + promedio);
+    } else {
+        alert("USTED APROBO " + promedio);
+    }
+} 
+*/
+/*
+
+let precio = parseFloat(prompt('INGRESAR PRECIO'));
+let descuento20 = precio - (precio * 0.2);
+
+alert ("con descuento es " + descuento20);
+*/
+
+//calcular triangulo
+/*
+var A =prompt("1 dato triangulo");
+var B = prompt ("2 dato triangulo");
+var resultado = (A*B) /2;
+alert("este es su resultado " + resultado);
+*/
+
+//calcular rectangulo
+/*
+var A =prompt("1 dato rectangulo");
+var B = prompt ("2 dato rectangulo");
+var resultado = (A*B);
+alert("este es su resultado " + resultado);
+*/
+
+
+//calcular circulo
+
+var pi=3.14159265359;
+var a;
+
+var r =prompt("ingrese el radio ");
+
+a=pi*(r*r);3
+alert("el area es "+a);
