3568ded7ffad7695b1fa37fa6860e3927feb23a1
Carlos Francisco <86804652+calitoozzy@users.noreply.github.com>
2022-04-20 16:14:34 -0300

    actualizacion hasta tp4

--- Unidad 4/js/code.js
+++ Unidad 4/js/code.js
@@ -1,2 +1,42 @@
 
+// 1-Programa con 2 numeros
 
+let numeroA = prompt("Ingrese el 1 Numero ");
+let numeroB = prompt("Ingrese el 2 Numero ");
+
+if (numeroA > numeroB) {
+    alert(numeroB + " Es menor que " + numeroA);
+} else
+    alert(numeroA + " Es menor que " + numeroB);
+
+
+//2-Identificar numeros pares/impares
+let numero = 0;
+numero = prompt("Indique un número: ", " ");
+if ((numero % 2) == 0) {
+    console.log(numero + " Es par ");
+} else {
+    console.log(numero + " Es impar ");
+}
+
+//3-Declarar una variable que se llame ganeLaCarrera
+let ganeLaCarrera = false;
+if (ganeLaCarrera == true) {
+    console.log("Ganaste");
+} else {
+    console.log("Perdiste");
+}
+
+//4-
+
+//5-
+
+//6-
+
+//7-
+
+//8-
+
+//9-
+
+//10-
