3a55c9fd773dbf508568bb8671937d8453a70469
Carlos Francisco <86804652+calitoozzy@users.noreply.github.com>
2022-04-25 16:10:12 -0300

    repositorio genereal

--- Unidad 4/js/code.js
+++ Unidad 4/js/code.js
@@ -1,6 +1,5 @@
-
 // 1-Programa con 2 numeros
-
+/*
 let numeroA = prompt("Ingrese el 1 Numero ");
 let numeroB = prompt("Ingrese el 2 Numero ");
 
@@ -29,14 +28,45 @@ if (ganeLaCarrera == true) {
 
 //4-
 
+let posicionEnLaCarrera = 6;
+
 //5-
 
+/*
 //6-
+let facturaTotal = prompt("ingrese el monto de su factura ");
+descuento = (facturaTotal * 0.20);
+seAplicaDescuento = 1000;
+if (facturaTotal > seAplicaDescuento) {
+    alert("Se aplico 20% de descuento su total es " + (facturaTotal - descuento));
+} else {
+    alert("Su compra no tiene descuento su total es " + facturaTotal);
+}
 
 //7-
 
+let montoVendido = prompt("Ingrese el monto vendido ");
+codigoDelVendedor = prompt("ingrese su codigo ");
+*/
+/*
 //8-
+let d = prompt("Ingrese el numero del dia en el que esta ");
+let dia1 = parseInt(1);
+dia2 = parseInt(2);
+if (d == 1)  alert("hoy es lunes") {
+    else if (d == 2) alert("hoy es martes")
+
+}
+*/
 
 //9-
 
 //10-
+let facturaTotal = prompt("ingrese el monto de su factura ");
+descuento = (facturaTotal * 0.15);
+seAplicaDescuento = 100;
+if (facturaTotal > seAplicaDescuento) {
+    alert("Se aplico 15% de descuento su total es " + (facturaTotal - descuento));
+} else {
+    alert("Su compra no tiene descuento su total es " + facturaTotal);
+}
