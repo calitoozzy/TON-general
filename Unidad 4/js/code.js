91056204a32903835cbe7bf18a860585c3d352fb
Carlos Francisco <86804652+calitoozzy@users.noreply.github.com>
2022-04-26 18:04:31 -0300

    correccion archivo js

--- Unidad 4/js/code.js
+++ Unidad 4/js/code.js
@@ -1,17 +1,18 @@
 // 1-Programa con 2 numeros
-/*
-let numeroA = prompt("Ingrese el 1 Numero ");
-let numeroB = prompt("Ingrese el 2 Numero ");
+
+let numeroA = parseInt(prompt("Ingrese el 1 Numero "));
+let numeroB = parseInt(prompt("Ingrese el 2 Numero "));
 
 if (numeroA > numeroB) {
     alert(numeroB + " Es menor que " + numeroA);
 } else
-    alert(numeroA + " Es menor que " + numeroB);
+    alert(numeroA + " Es menor que " + numeroB)
+
 
 
 //2-Identificar numeros pares/impares
 let numero = 0;
-numero = prompt("Indique un número: ", " ");
+numero = parseInt(prompt("Indique un número: ", " "));
 if ((numero % 2) == 0) {
     console.log(numero + " Es par ");
 } else {
@@ -32,9 +33,9 @@ let posicionEnLaCarrera = 6;
 
 //5-
 
-/*
+
 //6-
-let facturaTotal = prompt("ingrese el monto de su factura ");
+let facturaTotal = parseInt(prompt("ingrese el monto de su factura "));
 descuento = (facturaTotal * 0.20);
 seAplicaDescuento = 1000;
 if (facturaTotal > seAplicaDescuento) {
@@ -45,28 +46,33 @@ if (facturaTotal > seAplicaDescuento) {
 
 //7-
 
-let montoVendido = prompt("Ingrese el monto vendido ");
+let montoVendido = parseInt(prompt("Ingrese el monto vendido "));
 codigoDelVendedor = prompt("ingrese su codigo ");
-*/
-/*
+
+
 //8-
 let d = prompt("Ingrese el numero del dia en el que esta ");
-let dia1 = parseInt(1);
-dia2 = parseInt(2);
-if (d == 1)  alert("hoy es lunes") {
-    else if (d == 2) alert("hoy es martes")
 
-}
-*/
+if (d == 1) {
+    alert("hoy es lunes")
+} if (d == 2) {
+    alert("hoy es martes")
+} else if (d == 3) {
+    alert("hoy es martes")
+} else if 
+
+
+
 
 //9-
 
+
 //10-
-let facturaTotal = prompt("ingrese el monto de su factura ");
-descuento = (facturaTotal * 0.15);
+let recibos = prompt("ingrese el monto de su factura ");
+descuento = (recibos * 0.15);
 seAplicaDescuento = 100;
-if (facturaTotal > seAplicaDescuento) {
-    alert("Se aplico 15% de descuento su total es " + (facturaTotal - descuento));
+if (recibos > seAplicaDescuento) {
+    alert("Se aplico 15% de descuento su total es " + (recibos - descuento));
 } else {
-    alert("Su compra no tiene descuento su total es " + facturaTotal);
+    alert("Su compra no tiene descuento su total es " + recibos);
 }
